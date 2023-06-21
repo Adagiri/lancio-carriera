0b68f7904c7f14a6e30812b6fbf2117856638fec
adagiri <ibrahimridwan47@gmail.com>
2023-06-21 14:35:04 +0100

    ...

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,9 +10,9 @@ const {
 const { protectCompany, protectUser, protect } = require('../middlewares/auth');
 const router = express.Router();
 
-router.get('/company', protectCompany, getCompanyJobs);
-router.get('/', protectCompany, getJobs);
+router.get('/', protect, getJobs);
 router.get('/:id', protect, getJobById);
+router.get('/company', protectCompany, getCompanyJobs);
 router.post('/', protectCompany, postJob);
 router.post('/apply', protect, applyToJob);
 
