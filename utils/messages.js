cba88424d99f5d254e35c170148e6448a7e88df4
Ridwan Ibrahim <51293700+Adagiri@users.noreply.github.com>
2023-10-26 17:19:30 +0100

    Update messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -124,20 +124,54 @@ module.exports.sendResetPasswordEmailForUser = async (email, code) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moniedrop</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+        .container {
+            background-color: #ffffff;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            color: #007BFF;
+        }
+        p {
+            font-size: 16px;
+            color: #333;
+            margin: 10px 0;
+        }
+        .code {
+            font-size: 24px;
+            color: #007BFF;
+        }
+    </style>
+    <title>Lancio Carriera – Passwort zurücksetzen</title>
 </head>
 <body>
-    <p>Hi, please use the code below to reset your password. However, if you did not initiate a reset password request, please ignore this mail</p>
-    <p>>${code}</a></p>
-    <p>Please note that link expires in 10 minutes</p>
+    <div class="container">
+        <h1>Hallo,</h1>
+        <p>Verwenden Sie den folgenden Code, um Ihr Passwort zurückzusetzen:</p>
+        <p class="code">${code}</p>
+        <p>Wenn Sie dieses Zurücksetzen des Passworts nicht veranlasst haben, ignorieren Sie diese E-Mail bitte.</p>
+        <p>Bitte beachten Sie, dass der Reset-Link für die nächsten 10 Minuten gültig ist.</p>
+    </div>
 </body>
-</html>`;
+</html>
+`;
 
   try {
     const emailArgs = generateEmailArguments(
       null,
       email,
-      'Reset your Password',
+      'Setze dein Passwort zurück',
       message
     );
     await sendEmail(emailArgs);
@@ -153,20 +187,53 @@ module.exports.sendResetPasswordEmailForCompany = async (email, code) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moniedrop</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+        .container {
+            background-color: #ffffff;
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            color: #007BFF;
+        }
+        p {
+            font-size: 16px;
+            color: #333;
+            margin: 10px 0;
+        }
+        .code {
+            font-size: 24px;
+            color: #007BFF;
+        }
+    </style>
+    <title>Lancio Carriera – Passwort zurücksetzen</title>
 </head>
 <body>
-    <p>Hi, please use the code below to reset your password. However, if you did not initiate a reset password request, please ignore this mail</p>
-    <p>>${code}</a></p>
-    <p>Please note that link expires in 10 minutes</p>
+    <div class="container">
+        <h1>Hallo,</h1>
+        <p>Verwenden Sie den folgenden Code, um Ihr Passwort zurückzusetzen:</p>
+        <p class="code">${code}</p>
+        <p>Wenn Sie dieses Zurücksetzen des Passworts nicht veranlasst haben, ignorieren Sie diese E-Mail bitte.</p>
+        <p>Bitte beachten Sie, dass der Reset-Link für die nächsten 10 Minuten gültig ist.</p>
+    </div>
 </body>
-</html>`;
-
+</html>
+`;
   try {
     const emailArgs = generateEmailArguments(
       null,
       email,
-      'Reset your Password',
+      'Setze dein Passwort zurück',
       message
     );
     await sendEmail(emailArgs);
